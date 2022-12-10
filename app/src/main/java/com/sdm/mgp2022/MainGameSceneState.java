a0c1b2cdc2f313b9e927bc0455d20f122f3ed1c0
jiulen <wongjiulen.coding@gmail.com>
2022-12-10 16:16:29 +0800

    game ui background done

--- app/src/main/java/com/sdm/mgp2022/MainGameSceneState.java
+++ app/src/main/java/com/sdm/mgp2022/MainGameSceneState.java
@@ -12,7 +12,14 @@ import android.view.SurfaceView;
 // Created by TanSiewLan2021
 
 public class MainGameSceneState implements StateBase {
+    int ScreenWidth, ScreenHeight;
+    int tileWidth;
+
     private float timer = 0.0f;
+
+    //For ui background
+    Paint bgPaint = new Paint();
+    Paint bgOutlinePaint = new Paint();
 
     @Override
     public String GetName() {
@@ -24,7 +31,16 @@ public class MainGameSceneState implements StateBase {
     public void OnEnter(SurfaceView _view)
     {
         DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
-        int tileWidth = displayMetrics.widthPixels / 9;
+        ScreenWidth = displayMetrics.widthPixels;
+        ScreenHeight = displayMetrics.heightPixels;
+
+        bgPaint.setARGB(255, 82, 90, 107);
+        bgPaint.setStyle(Paint.Style.FILL);
+
+        bgOutlinePaint.setARGB(255, 255, 255, 255);
+        bgOutlinePaint.setStyle(Paint.Style.FILL);
+
+        tileWidth = ScreenWidth / 9;
 
         RenderBackground.Create();
         RenderTextEntity.Create();
@@ -34,8 +50,19 @@ public class MainGameSceneState implements StateBase {
         /* To spawn a tile
             TileEntity tile = TileEntity.Create(TileEntity.TILE_TYPES.TILE_METAL, tileWidth);
             tile.SetPosX(tile.GetWidth() * (column + 0.5f));
-            tile.SetPosY(tile.GetWidth() * (row + 0.5f));
+            tile.SetPosY(tile.GetWidth() * (row + 0.5f) + TOP_BAR_HEIGHT);
         */
+
+        //To visualise how much big the playing space is
+        /*for (int row=0; row < 10; ++row)
+        {
+            for (int col=0; col < 6; ++col)
+            {
+                TileEntity tile = TileEntity.Create(TileEntity.TILE_TYPES.TILE_METAL, tileWidth);
+                tile.SetPosX(tile.GetWidth() * (col + 0.5f));
+                tile.SetPosY(tile.GetWidth() * (row + 0.5f));
+            }
+        }*/
 
         board.fillBoard();
     }
@@ -50,6 +77,16 @@ public class MainGameSceneState implements StateBase {
     public void Render(Canvas _canvas)
     {
         EntityManager.Instance.Render(_canvas);
+
+        //Draw ui background
+        //Right
+        _canvas.drawRect(tileWidth * 6,0, ScreenWidth, tileWidth * 12, bgPaint);
+        //Left line of right box
+        _canvas.drawRect(tileWidth * 6,0, tileWidth * 6 + 10, tileWidth * 12, bgOutlinePaint);
+        //Bottom
+        _canvas.drawRect(0,tileWidth * 12, ScreenWidth, ScreenHeight, bgPaint);
+        //Top line of bottom box
+        _canvas.drawRect(0,tileWidth * 12, ScreenWidth, tileWidth * 12 + 10, bgOutlinePaint);
     }
 
     @Override
